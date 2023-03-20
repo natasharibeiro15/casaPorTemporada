4e879a37c497866f888501e2f853f4a10b371fde
Natasha Ribeiro <rhayane.pereira3432@gmail.com>
2023-03-20 15:02:26 -0400

    Login dos usuários

--- app/src/main/java/br/edu/ufam/casaportemporada/activity/autenticacao/LoginActivity.java
+++ app/src/main/java/br/edu/ufam/casaportemporada/activity/autenticacao/LoginActivity.java
@@ -4,10 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import br.edu.ufam.casaportemporada.R;
+import br.edu.ufam.casaportemporada.activity.MainActivity;
+import br.edu.ufam.casaportemporada.helper.FirebaseHelper;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+        private EditText edit_email, edit_senha;
+        private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +33,42 @@ public class LoginActivity extends AppCompatActivity {
 
         findViewById(R.id.text_recupera_conta).setOnClickListener(view ->
                 startActivity(new Intent(this, RecuperarContaActivity.class)));
+    }
+
+    public void validaDados(View view){
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        if(!email.isEmpty()){
+            if(!senha.isEmpty()){
+                progressBar.setVisibility(View.VISIBLE);
+                logar(email,senha);
+            }else{
+
+            }
+        }else{
+
+        }
+
+    }
+
+    private void logar(String email, String senha){
+        FirebaseHelper.getAuth().signInWithEmailAndPassword(
+          email,senha
+        ).addOnCompleteListener(task -> {
+            if(task.isSuccessful()){
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
+            }else{
+                String error = task.getException().getMessage();
+                Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void iniciaComponentes(){
+        edit_email = findViewById(R.id.edit_emailL);
+        edit_senha = findViewById(R.id.edit_senhaL);
+        progressBar = findViewById(R.id.progressBarL);
     }
 }
